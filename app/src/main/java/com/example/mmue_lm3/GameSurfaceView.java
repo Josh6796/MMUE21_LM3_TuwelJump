f92fbe87a21fc758928a0ebedfcdb826c106b716
Joshua Oblong <oblong.joshua@gmail.com>
2021-04-06 13:45:59 +0200

    Add GameLoop, GameSurfaceView and Scene with draw method

---
+++ app/src/main/java/com/example/mmue_lm3/GameSurfaceView.java
@@ -0,0 +1,70 @@
+package com.example.mmue_lm3;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+
+    private GameLoop gameLoop;
+    private Thread gameMainThread;
+
+    public GameSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getHolder().addCallback(this);
+        setFocusable(true);
+
+        loadAssets();
+    }
+
+    private void startGame(SurfaceHolder holder) {
+        gameLoop = new GameLoop(holder, this);
+        gameMainThread = new Thread(gameLoop);
+        gameMainThread.start();
+    }
+
+    private void endGame() {
+        gameLoop.setRunning(false);
+        try {
+            gameMainThread.join();
+        } catch (InterruptedException e) {
+            Log.e("Error", e.getMessage());
+        }
+    }
+
+    private void loadAssets() {
+        // Initialize the assets:
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        startGame(holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        endGame();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
+
---
+++ app/src/main/java/com/example/mmue_lm3/Scene.java
@@ -0,0 +1,11 @@
+package com.example.mmue_lm3;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+public class Scene {
+
+    public void draw(Canvas canvas) {
+        canvas.drawColor(Color.rgb(165, 200, 255));
+    }
+}
