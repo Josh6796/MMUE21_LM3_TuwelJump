30876fdac0ad271037035982bc540dffc789b195
Joshua Oblong <oblong.joshua@gmail.com>
2021-06-14 20:55:46 +0200

    Minor Fix for Ressource Management

--- app/src/main/java/com/example/mmue_lm3/GameLoop.java
+++ app/src/main/java/com/example/mmue_lm3/GameLoop.java
@@ -96,10 +96,6 @@ public class GameLoop implements Runnable, EventListener {
         this.running = running;
     }
 
-    public void setPaused(boolean pause) {
-        this.pause = pause;
-    }
-
     @Override
     public void run() {
         //One time Updates before first frame update is called
--- app/src/main/java/com/example/mmue_lm3/GameSurfaceView.java
+++ app/src/main/java/com/example/mmue_lm3/GameSurfaceView.java
@@ -14,6 +14,7 @@ import android.view.VelocityTracker;
 
 import com.example.mmue_lm3.events.EventSystem;
 import com.example.mmue_lm3.events.LevelEvent;
+import com.example.mmue_lm3.events.ResumeEvent;
 import com.example.mmue_lm3.events.TouchEvent;
 import com.example.mmue_lm3.events.VelocityEvent;
 import com.example.mmue_lm3.interfaces.Event;
@@ -51,7 +52,7 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
             gameMainThread = new Thread(gameLoop);
             gameMainThread.start();
         } else {
-            gameLoop.setPaused(false);
+            EventSystem.onEvent(new ResumeEvent());
         }
     }
 
